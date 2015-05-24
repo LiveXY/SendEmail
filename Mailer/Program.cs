2e6b692d2afb688889908590f09d513d70a3a6f1
hcxiong <cexo255@163.com>
2015-05-24 22:50:59 +0800

    增加命令行执行

---
+++ Mailer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TH.Mailer;
+using Pub.Class;
+
+namespace Mailer {
+    class Program {
+        static void Main(string[] args) {
+            Data.UsePool("ConnString");
+            MailerCenter.Start((msg) => {
+                msg = "[{0}]-{1}".FormatWith(DateTime.Now.ToDateTimeFFF(), msg);
+                Console.WriteLine(msg);
+                //Log.WriteLog(msg);
+            }, () => { Console.WriteLine("END"); });
+            Console.ReadKey();
+        }
+    }
+}
